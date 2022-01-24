8537231639e6fee496f65756c144507d0b568908
Brian O'Kelly <brianjrok@gmail.com>
2022-01-23 23:37:59 -0500

    Second Commit:
    Added saving and loading (not well). Should come back to clean up code and add visual elements to hangman.

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,15 @@
+require 'json'
+
 class Game
   def initialize(word)
+    puts 'Hello Player, what is your name?'
+    @player_name = gets.chomp
     @secret_word = word
     @guess_array = []
     @incorrect_array = []
     @hang_count = 0
     @victory = false
+    load_game
     play
   end
 
@@ -35,16 +40,48 @@ class Game
   end
 
   def turn
-    puts "Please enter a letter to guess or type \"save\" to save your game.\n The incorrect letters you have entered so far are #{@incorrect_array}"
+    puts "Please enter a letter to guess or type \"save\" to save your game.\n 
+    The incorrect letters you have entered so far are #{@incorrect_array}"
     letter_guess = gets.chomp.downcase
-    @guess_array.push(letter_guess)
-    if @secret_word.include? letter_guess
-      puts 'Correct!'
+    if letter_guess == "save"
+      save_game
     else
-      @incorrect_array.push(letter_guess)
-      @hang_count += 1
+      @guess_array.push(letter_guess)
+      if @secret_word.include? letter_guess
+        puts 'Correct!'
+      else
+        @incorrect_array.push(letter_guess)
+        @hang_count += 1
+      end
     end
   end
+
+  def save_game
+    Dir.mkdir('save_games') unless Dir.exist? ('save_games')
+    filename = "save_games/#{@player_name}.json"
+    tempHash = {
+      "player_name" => @player_name,
+      "secret_word" => @secret_word,
+      "guess_array" => @guess_array,
+      "incorrect_array" => @incorrect_array,
+      "hang_count" => @hang_count
+    }
+    File.open(filename, 'w') do |file|
+      file.write(tempHash.to_json)
+    end
+  end
+
+  def load_game
+    filename = "save_games/#{@player_name}.json"
+    if File.exist?(filename)
+      tempHash = JSON.parse(File.read(filename))
+      @secret_word = tempHash["secret_word"]
+      @guess_array = tempHash["guess_array"]
+      @incorrect_array = tempHash["incorrect_array"]
+      @hang_count = tempHash["hang_count"]
+    end
+  end
+
 end
 
 if File.exist? '5desk.txt'
